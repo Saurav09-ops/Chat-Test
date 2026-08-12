525e24182ddf5cd39f4fb066d82adf470899ae26
saurav <saurav.kc@gmail.com>
2026-08-12 19:59:25 +0545

    First Commit Chat-Test Project

---
+++ app.js
@@ -0,0 +1,262 @@
+import express from "express";
+import bodyParser from "body-parser";
+import pg from "pg";
+import { dirname } from "node:path";
+import { fileURLToPath } from "node:url";
+import { WebSocketServer } from "ws";
+import env from "dotenv";
+import { error } from "node:console";
+import http from "http";
+
+env.config();
+
+const app = express();
+const port = process.env.PORT || 3000;
+const server = http.createServer(app);
+const wss = new WebSocketServer({ server });
+
+let wsClients = [];
+
+const db = new pg.Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+  max: 10,
+  idleTimeoutMillis: 30000,
+});
+
+db.on("connect", (client) => {
+  client.query("SET search_path TO public");
+});
+
+db.query(
+  `
+  SELECT
+    current_database(),
+    current_user,
+    current_schema(),
+    current_setting('search_path')
+`,
+)
+  .then((result) => console.log("Database connection:", result.rows[0]))
+  .catch(console.error);
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+app.use(express.json());
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+app.get("/", async (req, res) => {
+  res.sendFile(`${__dirname}/public/index.html`);
+});
+
+wss.on("connection", (ws) => {
+  console.log("New client connected");
+
+  ws.on("message", async (message) => {
+    try {
+      const data = JSON.parse(message);
+
+      if (data.type === "identify") {
+        wsClients.push({ userId: data.userId, ws });
+        console.log(`User ${data.userId} connected`);
+        ws.userId = data.userId;
+        // ws.send(
+        //   JSON.stringify({
+        //     type: "welcome",
+        //     message: `Welcome user ${ws.userId}`,
+        //   }),
+        // );
+
+        wsClients.forEach((client) => {
+          client.ws.send(
+            JSON.stringify({
+              type: "statusUpdate",
+              userId: client.userId,
+            }),
+          );
+        });
+
+        console.log(`Current connected clients:${wsClients.length}`);
+      } else if (data.cmt) {
+        console.log(
+          `Received comment from user ${ws.userId}: ${data.cmt} to user ${data.Receiver}`,
+        );
+
+        let result = await db.query(
+          "insert into  messages (sender_id, reciver_id, content) values ($1, $2, $3) returning *",
+          [ws.userId, data.Receiver, data.cmt],
+        );
+
+        let msgData = result.rows[0];
+
+        wsClients.forEach((client) => {
+          if (
+            Number(client.userId) === msgData.reciver_id ||
+            Number(client.userId) === msgData.sender_id
+          ) {
+            client.ws.send(
+              JSON.stringify({
+                type: "chatMessage",
+                messageId: msgData.id,
+                senderId: msgData.sender_id,
+                msg: msgData.content,
+              }),
+            );
+          }
+        });
+      }
+    } catch (err) {
+      console.error("Error parsing message:", err.message);
+    }
+  });
+
+  ws.on("close", () => {
+    const index = wsClients.findIndex((client) => client.ws === ws);
+
+    if (index !== -1) {
+      const disconnectedClient = wsClients[index]; // get client first
+      console.log("================================");
+      wsClients.splice(index, 1);
+      console.log("Disconnected userId:", disconnectedClient.userId);
+    }
+    wsClients.forEach((client) => {
+      client.ws.send(
+        JSON.stringify({
+          type: "statusUpdate",
+          userId: client.userId,
+        }),
+      );
+    });
+  });
+});
+
+///////////////////////////////////////////////////////////////
+
+app.get("/log", async (req, res) => {
+  try {
+    let result = await db.query("SELECT * from public.users ORDER BY id ASC");
+
+    let posts = result.rows;
+
+    res.status(200).json({ posts });
+    console.log("dsfgsd>?");
+  } catch (er) {
+    console.log(er);
+  }
+});
+
+app.post("/verify", async (req, res) => {
+  try {
+    const { email } = req.body;
+    let result = await db.query("SELECT * from users ORDER BY id ASC");
+
+    const user = result.rows.find((post) => post.email === email);
+
+    if (user) {
+      return res.json({ access: true, userId: user.id });
+    }
+
+    return res.json({ access: false });
+  } catch (er) {
+    console.log(er);
+  }
+});
+
+app.get("/status", async (req, res) => {
+  try {
+    let status = [];
+    let result = await db.query("SELECT * from users ORDER BY id ASC");
+    let posts = result.rows;
+    // console.log(wsClients);
+
+    posts.forEach((post) => {
+      const exists = wsClients.some(
+        (client) => Number(client.userId) === Number(post.id),
+      );
+
+      let availability;
+
+      exists ? (availability = "Online") : (availability = "Offline");
+
+      console.log(`user ${post.id}: ${availability}`);
+
+      status.push({
+        userId: post.id,
+        first_name: post.first_name,
+        last_name: post.last_name,
+        email: post.email,
+        status: availability,
+      });
+    });
+
+    res.status(200).json({ data: status });
+  } catch (er) {
+    console.log(er);
+  }
+});
+
+app.post("/chat", async (req, res) => {
+  const user1 = req.body.senderId;
+  const user2 = req.body.receiverId;
+
+  const result = await db.query(
+    `SELECT *
+FROM messages
+WHERE
+    (sender_id = $1 AND reciver_id = $2)
+    OR
+    (sender_id = $2 AND reciver_id = $1)
+ORDER BY created_at ASC, id ASC
+LIMIT 50;`,
+    [user1, user2],
+  );
+
+  res.json(result.rows);
+});
+
+app.post("/reset-unread", async (req, res) => {
+  const receiverId = req.body.userId;
+  const senderId = req.body.receiverId;
+  try {
+    await db.query(
+      `UPDATE messages
+       SET read_at = NOW()
+       WHERE reciver_id = $1
+       AND sender_id = $2
+       AND read_at IS NULL`,
+      [receiverId, senderId],
+    );
+    res.status(200).json({ message: "successfully" });
+  } catch (error) {
+    console.error("Error resetting unread count:", error);
+    res.status(500).json({ message: "Error resetting unread count" });
+  }
+});
+
+app.post("/unread-count", async (req, res) => {
+  const userId = req.body.userId;
+  try {
+    const result = await db.query(
+      `SELECT
+  sender_id,
+  COUNT(*) AS unread_count
+FROM messages
+WHERE reciver_id = $1
+  AND read_at IS NULL
+GROUP BY sender_id
+ORDER BY sender_id;`,
+      [userId],
+    );
+    console.log("Unread count result:", result.rows);
+    res.json({ data: result.rows });
+  } catch (error) {
+    console.error("Error fetching unread count:", error);
+    res.status(500).json({ message: "Error fetching unread count" });
+  }
+});
+
+server.listen(port, "0.0.0.0", () => {
+  console.log(`Server is running on port ${port}`);
+});
---
+++ public/js/index.js
@@ -0,0 +1,472 @@
+let searchBar = document.querySelector(".search");
+let input = document.querySelector(".search-input");
+let verify = document.querySelector(".v");
+let Email = document.querySelector("#Email");
+let section = document.querySelector(".verify");
+let textarea = document.querySelector(".chat-input");
+let send = document.querySelector(".btn");
+const chatContainer = document.querySelector(".chat-c");
+let userID;
+let ws = null;
+let receiverId = null;
+
+document.querySelector(".chat-content").style.height =
+  `${document.querySelector(".chat").clientHeight - document.querySelector(".chat-util").offsetHeight}px`;
+
+verify.addEventListener("click", async () => {
+  const email = Email.value;
+  verification(email);
+});
+
+Email.addEventListener("keydown", async (e) => {
+  if (e.key === "Enter") {
+    const email = Email.value;
+    verification(email);
+  }
+});
+
+async function LoadMemberList(userId) {
+  const id = userId;
+  let result = await fetch(`/status`, {
+    method: "GET",
+    credentials: "include",
+  });
+  let value = await result.json();
+
+  let data = value.data;
+  console.log(data);
+
+  let a = "";
+
+  data.forEach((el) => {
+    if (el.userId !== id) {
+      a += `<div class="p" data-userid="${el.userId}">
+                <div class="p-pic">
+                  <img src="./assest/p1.jpeg" alt="" />
+                </div>
+                <div class="p-name">${el.first_name} ${el.last_name} <div class="status-${el.status}"></div> </div>
+                <div class="p-status">
+                  
+                 
+                 <div class="unread"></div>
+                </div>
+                
+              </div>`;
+    }
+  });
+  document.querySelector(".member-list").innerHTML = a;
+
+  await updateUnreadCounts(id);
+}
+
+async function updateUnreadCounts(userId) {
+  const response = await fetch("/unread-count", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      userId: userId,
+    }),
+  });
+  let unread = await response.json();
+  let unreadData = unread.data;
+  console.log(unreadData);
+
+  if (unreadData.length !== 0) {
+    unreadData.forEach((el) => {
+      const member = document.querySelector(
+        `.p[data-userid="${el.sender_id}"]`,
+      );
+      if (member) {
+        if (el.unread_count > 0) {
+          member.querySelector(".unread").style.display = "block";
+          member.querySelector(".unread").textContent = el.unread_count;
+        } else {
+          member.querySelector(".unread").style.display = "none";
+        }
+      }
+    });
+    return;
+  }
+  document.querySelectorAll(".unread").forEach((el) => {
+    el.style.display = "none";
+  });
+}
+
+// ws.addEventListener("open", () => {
+//   console.log("WebSocket connection opened");
+// });
+
+input.addEventListener("mouseover", () => {
+  searchBar.classList.add("hover");
+});
+
+input.addEventListener("mouseout", () => {
+  if (searchBar.classList.contains("hover")) {
+    searchBar.classList.remove("hover");
+  }
+});
+
+input.addEventListener("focus", () => {
+  if (searchBar.classList.contains("hover")) {
+    searchBar.classList.remove("hover");
+  }
+  searchBar.classList.add("active");
+});
+
+input.addEventListener("blur", () => {
+  searchBar.classList.remove("active");
+});
+
+textarea.addEventListener("input", () => {
+  if (textarea.scrollHeight <= 100) {
+    textarea.style.overflow = "hidden";
+    textarea.style.height = "auto";
+    textarea.style.height = `${textarea.scrollHeight}px`;
+    document.querySelector(".chat-content").style.height =
+      `${document.querySelector(".chat").clientHeight - document.querySelector(".chat-util").offsetHeight}px`;
+  } else {
+    textarea.style.overflow = "scroll";
+  }
+});
+
+const observer = new ResizeObserver((entries) => {
+  for (const entry of entries) {
+    console.log("New width:", entry.contentRect.width);
+    if (textarea.scrollHeight <= 100) {
+      textarea.style.overflow = "hidden";
+      textarea.style.height = "auto";
+      textarea.style.height = `${textarea.scrollHeight}px`;
+    } else {
+      textarea.style.overflow = "scroll";
+    }
+  }
+});
+
+observer.observe(textarea);
+
+// send.addEventListener("click", () => {
+//   console.log("clicked");
+//   if (textarea.value.trim() !== "") {
+//     let msgUser = ` <div>
+//                   <div class="c-user flex">
+//                     <div class="c-user-text">${textarea.value}</div>
+//                     <div class="c-pic">
+//                       <img src="./assest/p2.jpg" alt="" />
+//                     </div>
+//                   </div>
+//                 </div>
+//                 `;
+
+//     document.querySelector(".chat-c").innerHTML = msgUser;
+//   }
+// });
+
+// document.querySelector(".chat-input").addEventListener("keypress", (e) => {
+//   if (e.key === "Enter") {
+//     console.log("pressed");
+
+//     if (textarea.value.trim() !== "") {
+//       let msgNonUser = `<div class="bubble">
+//                   <div class="c-nonuser flex">
+//                     <div class="c-pic">
+//                       <img src="./assest/p1.jpeg" alt="" />
+//                     </div>
+//                     <div class="c-user-text">${textarea.value}</div>
+//                   </div>
+//                 </div>`;
+//       document.querySelector(".chat-c").innerHTML = msgNonUser;
+//     }
+//   }
+// });
+
+async function refreshChat(data, receiverId, userID) {
+  if (
+    Number(userID) === data.senderId ||
+    Number(receiverId) === data.senderId
+  ) {
+    if (data.senderId === userID) {
+      let msgUser = ` <div>
+                  <div class="c-user flex">
+                    <div class="c-user-text">${data.msg}</div>
+                    <div class="c-pic">
+                      <img src="./assest/p2.jpg" alt="" />
+                   </div>
+                  </div>
+                </div>
+                `;
+      chatContainer.insertAdjacentHTML("beforeend", msgUser);
+    } else if (data.senderId !== userID) {
+      let msgNonUser = `<div class="bubble">
+                  <div class="c-nonuser flex">
+                    <div class="c-pic">
+                      <img src="./assest/p1.jpeg" alt="" />
+                    </div>
+                    <div class="c-user-text">${data.msg}</div>
+                  </div>
+                </div>`;
+      chatContainer.insertAdjacentHTML("beforeend", msgNonUser);
+    }
+  } else {
+    await updateUnreadCounts(userID);
+  }
+
+  chatContainer.scrollTop = chatContainer.scrollHeight;
+}
+
+document.querySelector(".member-list").addEventListener("click", (e) => {});
+
+send.addEventListener("click", async (e) => {
+  const msg = textarea.value.trim();
+  if (msg === "") return;
+
+  try {
+    ws.send(JSON.stringify({ Receiver: receiverId, cmt: msg }));
+    textarea.value = "";
+  } catch (error) {
+    console.error("Error:", error);
+  }
+});
+
+textarea.addEventListener("keypress", async (e) => {
+  if (e.key === "Enter") {
+    e.preventDefault();
+    const msg = textarea.value.trim();
+    if (msg === "") return;
+
+    try {
+      ws.send(JSON.stringify({ Receiver: receiverId, cmt: msg }));
+      textarea.value = "";
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  }
+});
+
+async function verification(email) {
+  //  const email = Email.value;
+  if (email.trim() === "") {
+    console.log("email is empty");
+    return;
+  }
+  console.log("Email:", email);
+
+  const response = await fetch("/verify", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      email: email,
+    }),
+  });
+
+  const data = await response.json();
+  console.log(data);
+  userID = data.userId;
+  if (data.access) {
+    section.style.display = "none";
+    const protocol = window.location.protocol === "https:" ? "wss" : "ws";
+    ws = new WebSocket(`${protocol}://${window.location.host}`);
+    ws.addEventListener("open", () => {
+      console.log("WebSocket connection opened");
+      ws.send(
+        JSON.stringify({
+          type: "identify",
+          userId: `${data.userId}`,
+        }),
+      );
+    });
+    ws.addEventListener("message", (event) => {
+      let result = JSON.parse(event.data);
+      console.log(result.type);
+      if (result.type === "chatMessage") {
+        console.log(
+          `Received chat message from user ${result.senderId}: ${result.msg} (message ID: ${result.messageId})`,
+        );
+
+        refreshChat(result, receiverId, userID);
+      } else if (result.type === "statusUpdate") {
+        console.log(result);
+        console.log("Verification successful");
+        LoadMemberList(data.userId);
+      }
+    });
+  } else {
+    console.log("Verification failed");
+  }
+}
+
+// ws.addEventListener("message", (event) => {
+//   let result = event.data;
+//   console.log(result);
+//   console.log(userID);
+//   LoadMemberList(userID);
+// });
+
+async function relevantChat() {
+  let u = `<div>
+                  <div class="c-user flex">
+                    <div class="c-user-text">I am fine.</div>
+                    <div class="c-pic">
+                      <img src="./assest/p2.jpg" alt="" />
+                    </div>
+                  </div>
+                </div>
+                `;
+  let nu = `<div class="bubble">
+                  <div class="c-nonuser flex">
+                    <div class="c-pic">
+                      <img src="./assest/p1.jpeg" alt="" />
+                    </div>
+                    <div class="c-user-text">Hello, how are you?</div>
+                  </div>
+                </div>`;
+}
+// ----------------------------------------
+let wrapper = document.querySelector(".member-list");
+
+wrapper.addEventListener("click", async (e) => {
+  e.stopPropagation();
+
+  const member = e.target.closest(".p");
+  const memberPic = e.target.closest(".p-pic");
+
+  if (memberPic) {
+    console.log("pic clicked");
+    return;
+  }
+
+  if (member) {
+    receiverId = member.dataset.userid;
+
+    let name = member.querySelector(".p-name").innerHTML;
+    document.querySelector("#c-name").innerHTML = name;
+
+    await resetUnreadCount(userID, receiverId);
+    await fetchChat(userID, receiverId);
+
+    chatContainer.scrollTop = chatContainer.scrollHeight;
+
+    document.querySelector(".chat-cover").style.display = "none";
+
+    return;
+  }
+});
+
+async function resetUnreadCount(userId, receiverId) {
+  console.log(
+    "Resetting unread count for userId:",
+    userId,
+    "receiverId:",
+    receiverId,
+  );
+  try {
+    const response = await fetch("/reset-unread", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        userId: userId,
+        receiverId: receiverId,
+      }),
+    });
+
+    const result = await response.json();
+
+    if (result.message === "successfully") {
+      const member = document.querySelector(`.p[data-userid="${receiverId}"]`);
+      member.querySelector(".unread").style.display = "none";
+      console.log("Unread count reset successfully");
+    }
+  } catch (error) {
+    console.error("Error resetting unread count:", error);
+  }
+}
+
+async function fetchChat(userId, receiverId) {
+  chatContainer.innerHTML = `<div class="spacer"></div>`;
+  let msgUser = "";
+  let msgNonUser = "";
+
+  const response = await fetch("/chat", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      senderId: userId,
+      receiverId: receiverId,
+    }),
+  });
+
+  const data = await response.json();
+  console.log(data);
+
+  data.forEach((data) => {
+    if (data.sender_id === userID) {
+      msgUser = ` <div>
+                  <div class="c-user flex">
+                    <div class="c-user-text">${data.content}</div>
+                    <div class="c-pic">
+                      <img src="./assest/p2.jpg" alt="" />
+                   </div>
+                  </div>
+                </div>
+                `;
+      chatContainer.insertAdjacentHTML("beforeend", msgUser);
+    } else if (data.sender_id !== userID) {
+      msgNonUser = `<div class="bubble">
+                  <div class="c-nonuser flex">
+                    <div class="c-pic">
+                      <img src="./assest/p1.jpeg" alt="" />
+                    </div>
+                    <div class="c-user-text">${data.content}</div>
+                  </div>
+                </div>`;
+      chatContainer.insertAdjacentHTML("beforeend", msgNonUser);
+    }
+  });
+}
+
+// document.querySelector(".js-upload-btn").addEventListener("click", async () => {
+//   const img = document.querySelector(".js-upload-img");
+//   const input_files = document.querySelector(".js-upload");
+//   const titleInput = document.getElementById("titleInput");
+//   let file = input_files.files[0];
+//   if (!file) return alert("File is required");
+//   const title = titleInput.value;
+//   const path = URL.createObjectURL(file);
+//   const maxSize = 5 * 1024 * 1024;
+
+//   if (!file.type.startsWith("image/"))
+//     return alert("Only image files are allowed");
+//   if (file.size > maxSize) return alert("File is too large. Max size is 5MB");
+
+//   img.src = path;
+//   img.onload = () => URL.revokeObjectURL(path);
+
+//   const formData = new FormData();
+//   formData.append("file", file);
+
+//   formData.append("title", title);
+
+//   try {
+//     const res = await fetch("/imagePost", {
+//       method: "POST",
+//       body: formData,
+//       credentials: "include",
+//     });
+//     if (!res.ok) {
+//       throw new Error(`Upload failed with status ${res.status}`);
+//     }
+//     input_files.value = "";
+//     img.src = "";
+//     const result = await res.json();
+//     console.log(result);
+//   } catch (err) {
+//     console.error("Error uploading image: ", err);
+//   }
+// });
